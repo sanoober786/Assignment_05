008908cd60b5f54efddfa6b4a65f340ca508e432
sanoober <toobasadhayo454003@gmail.com>
2024-11-07 09:32:40 +0500

    pushing the code

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header"
+import Hero from "./component/Hero"
+import React from "react"
+
+ export default function Home(){
+  return(
+    <div>
+         <Header/>
+         <Hero/>
+    </div>
+  );
+}
+
